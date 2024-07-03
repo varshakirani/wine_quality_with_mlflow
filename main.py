81e042723ece020268092c92ff21ca297bb63630
varsha@mac <varshakirani@gmail.com>
2024-07-03 15:57:28 +0200

    Add: model trainer pipeline

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mlflow_project import logger
 from mlflow_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlflow_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlflow_project.pipeline.stage_03_data_transformation import DataTransformationPipeline
+from mlflow_project.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -33,6 +34,17 @@ try:
     obj = DataTransformationPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:
     logger.exception(e)
     raise e
---
+++ src/mlflow_project/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,26 @@
+from mlflow_project.config.configuration import ConfigurationManager
+from mlflow_project.components.model_trainer import ModelTrainer
+from mlflow_project import logger
+
+STAGE_NAME = "Model Trainer stage"
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer = ModelTrainer(config=model_trainer_config)
+        model_trainer.train()
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
