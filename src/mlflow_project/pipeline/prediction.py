20aebf394712e28f0c2c688e682cf31ba2432a36
varsha@mac <varshakirani@gmail.com>
2024-07-08 23:52:47 +0200

    Add: prediction pipeline and flask app with basic website ui

---
+++ src/mlflow_project/pipeline/prediction.py
@@ -0,0 +1,10 @@
+import joblib
+from pathlib import Path
+
+class PredictionPipeline:
+    def __init__(self):
+        self.model = joblib.load(Path("artifacts/model_trainer/model.joblib"))
+
+    def predict(self, data):
+        prediction = self.model.predict(data)
+        return prediction
